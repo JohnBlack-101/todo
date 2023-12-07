9e5363ec73172a2a93dc85de6c6215dcf2961066
John Black <john.m.black.16@gmail.com>
2023-12-07 00:00:37 -0600

    React App Created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+//React Hooks allow us to manage any changes in a component. When we need to manage those changes, we need React hooks. To implement, we add useState to the React import below
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  //Below we will create a React Hook, which is a variable that is created as the component is mounted to the Virtual DOM. We set the state of showContent to equal 'Home' as its initial state. setShowContent() is a function that we can use to manage the state of showContent. We should never try to directly change the value of showContent, but instead we use setShowContent to manage the 'state' of the variable whenever we need to change its value.
+  const [showContent, setShowContent] = useState('Home')
+
+  return 
 }
-
-export default App;
